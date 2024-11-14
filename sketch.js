64f2e5cefa66d894aaa0936ae7ed72fec76f5da2
Gabe Johnson <gajo2539@colorado.edu>
2024-11-14 14:33:22 -0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background('blue');
+  stroke('white');
+  ellipse(width / 2, height * .8, 100, 120);
+  ellipse(width / 2, height * .6, 80, 70);
+}
